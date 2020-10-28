cad9f33982c38c86610bd23dc6034636115a4993
SureVX <ben@surevx.com>
2020-10-28 11:13:16 +0000

    Initial commit

---
+++ src/Summary.ts
@@ -0,0 +1,32 @@
+import { MatchData } from "./MatchData";
+import { WinsAnalysis } from './Analyzers/WinsAnalysis';
+import { HtmlReport } from './HtmlReport';
+
+export interface Analyzer {
+    run(matches: MatchData[]): string;
+}
+
+export interface OutputTarget {
+    print(report: string): void;
+}
+
+export class Summary {
+    static winsAnalysisWithHtmlReport(team: string): Summary {
+        return new Summary(
+            new WinsAnalysis(team),
+            new HtmlReport()
+        )
+    }
+
+    constructor(
+        public analyzer: Analyzer, 
+        public outputTarget: OutputTarget) {}
+
+    buildAndPrintReport(matches: MatchData[]): void {
+        const output = this.analyzer.run(matches);
+        this.outputTarget.print(output);
+    }
+}
+
+
+
